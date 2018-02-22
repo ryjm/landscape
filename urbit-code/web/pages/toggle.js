d29d35408ed752f32968d217e7a1662c5fcfb20f
Anthony <aarroyo@appliedminds.com>
2018-02-21 17:30:15 -0800

    add url hash fragment when entering edit mode.

--- urbit-code/web/pages/toggle.js
+++ urbit-code/web/pages/toggle.js
@@ -1,15 +1,16 @@
-document.toggleDisplay = function(id1, id2) {
+document.toggleDisplay = function() {
   var id1 = 'show';
   var id2 = 'edit';
   var isDisplayed = function(id) {
     return document.getElementById(id).style.display != 'none';
   }
-  console.log(document.getElementById(id1));
-  console.log(document.getElementById(id2));
   if (isDisplayed(id1)) {
+    window.location.href = window.location.href + '#' + id2
     document.getElementById(id1).style.display = 'none';
     document.getElementById(id2).style.display = 'inherit';
   } else {
+    window.location.href = window.location.href + '#' + id1
+    document.getElementById(id1).style.display = 'none';
     document.getElementById(id1).style.display = 'inherit';
     document.getElementById(id2).style.display = 'none';
   }
