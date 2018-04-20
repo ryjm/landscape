d32611b267cd95a47e00b22b89c4e0eae268448d
Anthony <anthony@tlon.io>
2018-04-20 16:59:35 -0700

    updated transitions from comment component (needed update to urbit-reports)

--- src/js/components/collection/comment.js
+++ src/js/components/collection/comment.js
@@ -21,9 +21,14 @@ export class CommentCreate extends Component {
         com: '~',
         wat: this.state.comment
       }
-    }, {
-      target: `/~~/collections/${this.props.coll}/${this.props.top}`
-  });
+    });
+
+    this.props.pushPending("circle.gram", {
+      type: "transition",
+      data: {
+        target: `/~~/collections/${this.props.coll}/${this.props.top}`
+      }
+    });
   }
 
   valueChange(event) {
