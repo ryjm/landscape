24e4b1be4651e7a496bf4a6c78cfd2dfc93f30ed
Chris Tlon <chris@Chriss-MBP.20mission>
2018-06-25 21:03:56 -0700

    Got some form inputs on the screen for collection-create

--- src/js/components/command/collection-create.js
+++ src/js/components/command/collection-create.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from '/components/lib/button';
 
 export class CollectionCreate extends Component {
   constructor(props) {
@@ -13,7 +14,45 @@ export class CollectionCreate extends Component {
 
   render() {
     return (
-      <h5>Disabled screen</h5>
+      <div className="mb-3">
+        <div className="input-group">
+          <label>Name</label>
+          <input type="text"
+            name="name"
+            onChange={this.onValueChange}
+            value={this.state.name}
+            placeholder="Deep Thoughts" />
+        </div>
+        <div className="input-group">
+          <label>Invites</label>
+          <textarea type="text"
+            name="invites"
+            onChange={this.onValueChange}
+            value={this.state.invites}
+            placeholder="~ship-name" />
+        </div>
+        <div className="input-group">
+          <label>Show in Profile?</label>
+          <textarea type="text"
+            name="visible"
+            onChange={this.onValueChange}
+            value={this.state.visible}
+            placeholder="'yes' or 'no'" />
+        </div>
+        <Button
+          classes={`btn btn-sm btn-primary`}
+          action={this.create}
+          content="Create →"
+          pushCallback={this.props.pushCallback}
+          responseKey="circles"
+           />
+       <Button
+         classes={`btn btn-sm btn-secondary`}
+         action={this.cancel}
+         content="Cancel"
+         pushCallback={this.props.pushCallback}
+          />
+      </div>
     )
   }
 }
