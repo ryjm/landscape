73fcff5c2d8b2d8a46fb5f58f6741fa599a84a04
Anthony Arroyo <anthony@tlon.io>
2018-07-19 16:05:29 -0700

    grey out pending comment

--- src/js/components/collection/comment.js
+++ src/js/components/collection/comment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TRANSITION_LOADING } from '/lib/constants';
+import { TRANSITION_LOADING, STATUS_READY, STATUS_LOADING } from '/lib/constants';
 import { Button } from '/components/lib/button';
 
 export class CommentCreate extends Component {
@@ -8,13 +8,16 @@ export class CommentCreate extends Component {
     this.createComment = this.createComment.bind(this);
     this.valueChange = this.valueChange.bind(this);
     this.state = {
-      comment: ''
+      comment: '',
+      status: STATUS_READY
     };
     let loc = window.location.pathname;
     this.pageShip = loc.includes("/==/web") ? loc.split('/')[2] : `~${props.api.authTokens.ship}`
   }
 
   createComment() {
+    this.setState({ status: STATUS_LOADING });
+
     this.props.api.coll({
       comment: {
         col: this.props.coll,
@@ -30,10 +33,9 @@ export class CommentCreate extends Component {
       data: TRANSITION_LOADING
     }]);
 
-    // I hate this
-    this.setState({comment: ''});
-
     this.props.pushCallback("circle.gram", (rep) => {
+      this.setState({ comment: '', status: STATUS_READY});
+
       this.props.transitionTo(this.pageShip == this.props.api.authTokens.ship ? `/~~/collections/${this.props.coll}/${this.props.top}` : `/~~/${this.pageShip}/==/web/collections/${this.props.coll}/${this.props.top}`)
     });
   }
@@ -60,11 +62,12 @@ export class CommentCreate extends Component {
           onChange={this.valueChange}
           name="comment"
           className="comment-edit mb-3"
+          disabled={this.state.status === STATUS_LOADING}
           placeholder="Post a comment">
         </textarea>
         <Button
           classes="btn btn-tetiary"
-          disabled={this.state.comment.length === 0}
+          disabled={this.state.comment.length === 0 || this.state.status === STATUS_LOADING}
           action={this.createComment}
           responseKey="circle.gram"
           pushCallback={this.props.pushCallback}
