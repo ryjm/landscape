d90915b1d164e69998076a89180a1f62a9f62264
Chris Johnson <chris@cjohnson.io>
2018-08-14 10:16:29 -0700

    - Don’t let other states override the “disconnected” state

--- src/js/reducers/views.js
+++ src/js/reducers/views.js
@@ -1,11 +1,17 @@
-import { REPORT_PAGE_STATUS } from '/lib/constants';
+import { REPORT_PAGE_STATUS, PAGE_STATUS_DISCONNECTED, PAGE_STATUS_READY } from '/lib/constants';
 
 export class ViewsReducer {
   reduce(reports, store) {
     reports.forEach((rep) => {
       switch (rep.type) {
         case REPORT_PAGE_STATUS:
-          store.views.transition = rep.data;
+          // Don't let any state other than "READY" override the disconnected state
+          let isDisconnected = store.views.transition === PAGE_STATUS_DISCONNECTED;
+          let setToReady = rep.data === PAGE_STATUS_READY;
+
+          if (!isDisconnected || setToReady) {
+            store.views.transition = rep.data;
+          }
           break;
       }
     });
