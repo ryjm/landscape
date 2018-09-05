69678440e63a30e31976be554483cf34933d6430
Chris Johnson <chris@cjohnson.io>
2018-09-05 14:53:01 -0700

    - Implement Sigils

--- src/js/components/lib/seal-dict.js
+++ src/js/components/lib/seal-dict.js
@@ -1,4 +1,65 @@
-import Seals from '/vendor/seals-0.1.1';
+import React, { Component } from 'react';
+import { pour } from '/vendor/sigils-0.1.1';
+import _ from 'lodash';
+
+const ReactSVGComponents = {
+  svg: p => {
+    return (
+      <svg {...p.attr} version={'1.1'} xmlns={'http://www.w3.org/2000/svg'}>
+       { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </svg>
+    )
+  },
+  circle: p => {
+    return (
+      <circle {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </circle>
+    )
+  },
+  rect: p => {
+    return (
+      <rect {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </rect>
+    )
+  },
+  path: p => {
+    return (
+      <path {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </path>
+    )
+  },
+  g: p => {
+    return (
+      <g {...p.attr}>
+        { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </g>
+    )
+  },
+  polygon: p => {
+    return (
+      <polygon {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </polygon>
+    )
+  },
+  line: p => {
+    return (
+      <line {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </line>
+    )
+  },
+  polyline: p => {
+    return (
+      <polyline {...p.attr}>
+      { _.map(_.get(p, 'children', []), child => ReactSVGComponents[child.tag](child)) }
+      </polyline>
+    )
+  }
+}
 
 export class SealDict {
   constructor() {
@@ -9,7 +70,7 @@ export class SealDict {
     let key = `${patp}+${size}`;
 
     if (!this.dict[key]) {
-      this.dict[key] = Seals.pourReact(patp, size)
+      this.dict[key] = pour({patp, size, renderer: ReactSVGComponents})
     }
 
     return this.dict[key];
