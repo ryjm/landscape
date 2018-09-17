34f0c366ea1365a10866b01b1948a93fe728ab1b
Chris Johnson <chris@cjohnson.io>
2018-09-17 14:10:29 -0700

    - fix page-status positioning in command page

--- src/js/components/lib/icons/icon-comment.js
+++ src/js/components/lib/icons/icon-comment.js
@@ -8,7 +8,7 @@ export class IconComment extends Component {
         <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="2" y="2" width="12" height="12">
           <path fillRule="evenodd" clipRule="evenodd" d="M9.2 10.4L14 14V2H2V10.4H9.2ZM3.2 9.2H9.35486C9.48986 9.2 9.62096 9.24554 9.72686 9.32924L12.8 11.7578V3.2H3.2V9.2Z" fill="black"/>
           <path d="M3.2 9.2H9.35486C9.48986 9.2 9.62096 9.24554 9.72686 9.32924L12.8 11.7578V3.2H3.2V9.2Z" fill="black"/>
-          </mask>
+        </mask>
         <g mask="url(#mask0)">
           <rect width="16" height="16" fill="black"/>
         </g>
