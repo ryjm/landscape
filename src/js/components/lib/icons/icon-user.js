102d141b22549286a860fc0d313db2cdbfeda595
Jimmy <jyng02@gmail.com>
2018-10-05 12:10:06 -0700

    Inbox list complete
    https://app.asana.com/0/704904477897839/843288868761154/f
    
    removing white fills for all icons.

--- src/js/components/lib/icons/icon-user.js
+++ src/js/components/lib/icons/icon-user.js
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 export class IconUser extends Component {
   render() {
     return (
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="16" height="16" fill="white"/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M6.5 5.5C6.5 4.67157 7.17163 4 8 4C8.82835 4 9.5 4.67157 9.5 5.5C9.5 6.32843 8.82835 7 8 7C7.17163 7 6.5 6.32843 6.5 5.5ZM5.5 5.5C5.5 4.11926 6.61927 3 8 3C9.38075 3 10.5 4.11926 10.5 5.5C10.5 6.88073 9.38075 8 8 8C6.61927 8 5.5 6.88073 5.5 5.5ZM12.5 12.5H11.5C11.5 10.9506 10.047 9.5 8 9.5C5.95301 9.5 4.5 10.9506 4.5 12.5H3.5C3.5 10.2908 5.51477 8.5 8 8.5C10.4852 8.5 12.5 10.2908 12.5 12.5Z" fill="black"/>
-      </svg>
+      <svg fill="none" height="16" width="16" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="m8 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm4.667 12h1.333c0-2.761-2.686-5-6-5s-6 2.239-6 5z" fill="#000" fill-rule="evenodd"/></svg>
     )
   }
 }
