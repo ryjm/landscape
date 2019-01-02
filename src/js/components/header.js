c4b88d871b45d3978483971e7486439210c7d81a
Chris Johnson <chris@cjohnson.io>
2019-01-02 08:52:27 -0600

    - Initial style for header notifications

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -365,7 +365,9 @@ export class Header extends Component {
           </div>
         </div>
         <div className="row align-center header-mainrow">
-          <div className="flex-col-1"></div>
+          <div className="flex-col-1 flex justify-end">
+            <div className="header-notifications text-mono text-700">3</div>
+          </div>
           <div className="flex-col-1 flex space-between align-center">
             <a onClick={this.toggleMenu}>
               <Icon type="icon-panini" />
